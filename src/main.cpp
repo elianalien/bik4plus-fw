1cf8d3802a9b62d665dc031cb19b7ba492c97e19
Tuwuh Sarwoprasojo <tuwuhs@gmail.com>
2017-06-03 00:39:30 +0700

    Enable EXTI on PB11 for wake up. Proper sleep sequence.

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@
 #include "util/Ticks.h"
 
 #include "stm32f10x.h"
+#include "stm32f10x_exti.h"
 #include "stm32f10x_pwr.h"
 #include "stm32f10x_rtc.h"
 
@@ -27,6 +28,7 @@ void initGpio()
 {
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
 
 	GPIO_InitTypeDef GPIO_InitStruct;
 
@@ -42,6 +44,21 @@ void initGpio()
 	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_Init(GPIOA, &GPIO_InitStruct);
 	RCC_MCOConfig(RCC_MCO_SYSCLK);
+
+	// External event
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_11;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IPU;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_2MHz;
+	GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+	GPIO_EXTILineConfig(GPIO_PortSourceGPIOB, GPIO_PinSource11);
+
+	EXTI_InitTypeDef EXTI_InitStruct;
+	EXTI_InitStruct.EXTI_Line = EXTI_Line11;
+	EXTI_InitStruct.EXTI_LineCmd = ENABLE;
+	EXTI_InitStruct.EXTI_Mode = EXTI_Mode_Event;
+	EXTI_InitStruct.EXTI_Trigger = EXTI_Trigger_Falling;
+	EXTI_Init(&EXTI_InitStruct);
 }
 
 int main(int argc, char* argv[])
@@ -52,17 +69,24 @@ int main(int argc, char* argv[])
 
 	// Infinite loop
 	while (1) {
+//		GPIO_WriteBit(GPIOB, GPIO_Pin_12, (BitAction) GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_11));
+		GPIO_WriteBit(GPIOB, GPIO_Pin_12, Bit_SET);
 		for (unsigned count = 0; count < 10; count++) {
 			GPIO_WriteBit(GPIOB, GPIO_Pin_12,
 					(BitAction) !GPIO_ReadOutputDataBit(GPIOB, GPIO_Pin_12));
 
-			Ticks::DelayMs(250);
+			Ticks::DelayMs(100);
 		}
 
 //		RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, DISABLE);
 //		RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, DISABLE);
 
-//		__WFE();
+		// Based on http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.faqs/ka15506.html
+		// The sequence SEV WFE WFE is used to ensure sleep
+		SCB->SCR &= ~SCB_SCR_SLEEPDEEP;
+		__SEV();
+		__WFE();
+		__WFE();
 //		PWR_EnterSTOPMode(PWR_Regulator_LowPower, PWR_STOPEntry_WFE);
 //		PWR_EnterSTANDBYMode();
 	}
